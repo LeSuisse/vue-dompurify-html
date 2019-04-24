778aa296ede4981298023b75cc50806a7b8710d2
Thomas Gerbet <thomas.gerbet@enalean.com>
2019-04-24 13:39:44 +0200

    Version 1.2.1

--- dist/dompurify-html.js
+++ dist/dompurify-html.js
@@ -4,6 +4,9 @@ var dompurify_1 = require("dompurify");
 function buildDirective(config) {
     if (config === void 0) { config = {}; }
     var updateComponent = function (el, binding) {
+        if (binding.oldValue === binding.value) {
+            return;
+        }
         var arg = binding.arg;
         if (arg in config) {
             el.innerHTML = dompurify_1.sanitize(binding.value, config[arg]);
