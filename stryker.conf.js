6cec7a8d1fadcb3845d5772c0fd1c9c03baafa62
Thomas Gerbet <thomas@gerbet.me>
2019-08-15 19:55:45 +0200

    Move to Jest

--- stryker.conf.js
+++ stryker.conf.js
@@ -3,12 +3,8 @@ module.exports = config => {
         mutator: 'typescript',
         packageManager: 'npm',
         reporters: ['clear-text', 'progress', 'dashboard'],
-        testRunner: 'karma',
-        testFramework: 'jasmine',
+        testRunner: 'jest',
         coverageAnalysis: 'off',
-        karma: {
-            configFile: 'karma.conf.ts'
-        },
         tsconfigFile: 'tsconfig.json',
         mutate: ['src/**/*.ts'],
         thresholds: {
