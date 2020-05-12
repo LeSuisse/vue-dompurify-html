1f4e3cfdce6fb62d8b7789d2f07195fe1cbd2559
Thomas Gerbet <thomas@gerbet.me>
2020-05-13 00:39:51 +0200

    Version 3.0.0-alpha.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,6 @@ import { App } from 'vue';
 import { DirectiveConfig, MinimalDOMPurifyConfig } from './dompurify-html';
 export { DirectiveConfig, MinimalDOMPurifyConfig };
 declare const _default: {
-    install(app: App<any>, config?: DirectiveConfig): void;
+    install(app: App, config?: DirectiveConfig): void;
 };
 export default _default;
