0604e1b2fa3cf485595a26cb7a01aecfd16a9ce9
Thomas Gerbet <thomas@gerbet.me>
2021-12-10 15:28:52 +0100

    Version 3.0.0-beta.1

--- dist/vue-dompurify-html.es.js
+++ dist/vue-dompurify-html.es.js
@@ -1,1 +1,37 @@
-import n from"dompurify";var o={install(o,t={}){o.directive("dompurify-html",function(o={}){const t=n();!function(n,o){const t=n.hooks;let i;for(i in t)o.addHook(i,t[i])}(o,t);const i=function(n,i){const e=i.arg,r=o.namedConfigurations;r&&void 0!==r[e]?n.innerHTML=t.sanitize(i.value,r[e]):n.innerHTML=t.sanitize(i.value,o.default)};return{mounted:i,updated:i}}(t))}};export default o;
+import dompurify from "dompurify";
+function setUpHooks(config, dompurifyInstance) {
+  var _a;
+  const hooks = (_a = config.hooks) != null ? _a : {};
+  let hookName;
+  for (hookName in hooks) {
+    const hook = hooks[hookName];
+    if (hook !== void 0) {
+      dompurifyInstance.addHook(hookName, hook);
+    }
+  }
+}
+function buildDirective(config = {}) {
+  const dompurifyInstance = dompurify();
+  setUpHooks(config, dompurifyInstance);
+  const updateComponent = function(el, binding) {
+    var _a, _b;
+    const arg = binding.arg;
+    const namedConfigurations = config.namedConfigurations;
+    const defaultConfig = (_a = config.default) != null ? _a : {};
+    if (namedConfigurations && arg !== void 0) {
+      el.innerHTML = dompurifyInstance.sanitize(binding.value, (_b = namedConfigurations[arg]) != null ? _b : defaultConfig);
+      return;
+    }
+    el.innerHTML = dompurifyInstance.sanitize(binding.value, defaultConfig);
+  };
+  return {
+    mounted: updateComponent,
+    updated: updateComponent
+  };
+}
+var index = {
+  install(app, config = {}) {
+    app.directive("dompurify-html", buildDirective(config));
+  }
+};
+export { index as default };
