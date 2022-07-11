96a83593499f8e5242a4346a30efa112661cb441
Thomas Gerbet <thomas@gerbet.me>
2022-07-11 23:29:07 +0200

    Migrate to pnpm

--- stryker.conf.js
+++ stryker.conf.js
@@ -3,6 +3,7 @@ module.exports = {
     reporters: ['clear-text', 'progress', 'dashboard'],
     testRunner: 'jest',
     coverageAnalysis: 'off',
+    plugins: ["@stryker-mutator/typescript-checker", "@stryker-mutator/jest-runner"],
     checkers: ["typescript"],
     tsconfigFile: 'tsconfig.json',
     mutate: ['src/**/*.ts'],
