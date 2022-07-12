0effc96323fdc768147b7954ecf2e584d4466900
Thomas Gerbet <thomas@gerbet.me>
2022-07-12 22:35:39 +0200

    Reorganize into a monorepo structure

--- examples/vue3/src/Example.vue
+++ examples/vue3/src/Example.vue
@@ -3,11 +3,12 @@
     <div v-dompurify-html="rawHtml">Expect text with color</div>
     <div v-dompurify-html:plaintext="rawHtml">Expect only plaintext</div>
     <h1>Directive local to a component</h1>
-    <example-local-directive />
+    <example-local-directive></example-local-directive>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import ExampleLocalDirective from "./ExampleLocalDirective.vue";
 
-const rawHtml = '<span style="color: red">Hello!</span><img src=a onerror="alert(1)">';
+const rawHtml = ref('<span style="color: red">Hello!</span><img src=a onerror="alert(1)">');
 </script>
---
+++ examples/vue3/src/ExampleLocalDirective.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-clean-html="rawHtml">Expect text with color</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { buildVueDompurifyHTMLDirective } from 'vue-dompurify-html';
+
+const vCleanHtml = buildVueDompurifyHTMLDirective();
+const rawHtml = ref('<span style="color: red">Hello!</span><img src=a onerror="alert(1)">');
+</script>
