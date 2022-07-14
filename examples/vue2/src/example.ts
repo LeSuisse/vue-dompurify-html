49f7240f4c6c8c395ea2d3b86d5d1b4cf97cedbe
Thomas Gerbet <thomas@gerbet.me>
2022-07-14 14:31:55 +0200

    Add a Vue 2 example

---
+++ examples/vue2/src/example.ts
@@ -0,0 +1,13 @@
+import VueDOMPurifyHTML from 'vue-dompurify-html';
+import Example from './Example.vue';
+import Vue from "vue";
+
+Vue.use(VueDOMPurifyHTML, {
+    namedConfigurations: {
+        plaintext: {
+            USE_PROFILES: { html: false },
+        },
+    },
+});
+const App = Vue.extend(Example);
+new App().$mount("#app");
