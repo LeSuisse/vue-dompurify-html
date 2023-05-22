32d1dad14a0bc6f79cf8a5f3079a9453e39ca6fd
Thomas Gerbet <thomas@gerbet.me>
2023-05-22 22:02:09 +0200

    Bump nixpkgs pin

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (
   fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/2362848adf8def2866fabbffc50462e929d7fffb.tar.gz";
-    sha256 = "0wjr874z2y3hc69slaa7d9cw7rj47r1vmc1ml7dw512jld23pn3p";
+    url = "https://github.com/NixOS/nixpkgs/archive/9356eead97d8d16956b0226d78f76bd66e06cb60.tar.gz";
+    sha256 = "1f6gqsm9f8hx3sf5a66iih619bfzzvs5xdval3p807ad8a1zwmbq";
   }
 ) {} }:
 
