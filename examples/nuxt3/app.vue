9f6eb6c0ae2611728640cb2d25ff08dbaebdb2d2
Thomas Gerbet <thomas@gerbet.me>
2023-12-15 15:01:19 +0100

    Add Nuxt 3 example

---
+++ examples/nuxt3/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Global directive</h1>
+    <div v-dompurify-html="rawHtml">Expect text with color</div>
+    <div v-dompurify-html:plaintext="rawHtml">Expect only plaintext</div>
+    <h1>Directive local to a component</h1>
+    <div v-clean-html="rawHtml">Expect text with color</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { buildVueDompurifyHTMLDirective } from 'vue-dompurify-html';
+
+const rawHtml = ref('<span style="color: red">Hello!</span><img src=a onerror="alert(1)">');
+
+const vCleanHtml = buildVueDompurifyHTMLDirective();
+</script>
