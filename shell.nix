3723fa26888eecf5f7e9f3b5914cf18e070acc06
Thomas Gerbet <thomas@gerbet.me>
2024-03-24 11:55:22 +0100

    Update dev/build env [W12 2024]

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (
   fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/e97b3e4186bcadf0ef1b6be22b8558eab1cdeb5d.tar.gz";
-    sha256 = "sha256:114ggf0xbwq16djg4qql3jljknk9xr8h7dw18ccalwqg9k1cgv0g";
+    url = "https://github.com/NixOS/nixpkgs/archive/e1d501922fd7351da4200e1275dfcf5faaad1220.tar.gz";
+    sha256 = "sha256:085jmvkr1r1ag18mp2skf9nrap3i3gwphlf7zaagwfh9q11lj13l";
   }
 ) {} }:
 
