defaadc4642cd0680802af8b9bd372aa236f003e
Thomas Gerbet <thomas@gerbet.me>
2025-05-05 12:31:26 +0200

    chore: Update nixpkgs pin

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (
   fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/de0fe301211c267807afd11b12613f5511ff7433.tar.gz";
-    sha256 = "sha256:0lw17azfdh3gmh75ddha01j4c7fn38nz4w3jwzbaz0ngb8nchb3a";
+    url = "https://github.com/NixOS/nixpkgs/archive/6b1c028bce9c89e9824cde040d6986d428296055.tar.gz";
+    sha256 = "sha256:02zz81ni61y53mrgd42r0fgpwg26rsn098a1100zdx5h9558l6al";
   }
 ) {} }:
 
